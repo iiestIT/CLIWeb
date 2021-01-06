938402684e5a650d28bebc28c0836894e288054d
iiestIT <it.iiest.de@gmail.com>
2021-01-06 12:19:01 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CLIWeb',
+    version='0.0.1',
+    packages=['CLIWeb'],
+    url='https://github.com/iiestIT/CLIWeb',
+    license='MIT',
+    author='iiestIT',
+    author_email='it.iiest.de@gmail.com',
+    description='a cli tool to performe http requests and socket connections.'
+)
